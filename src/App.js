9cbb7dd7e293ff1d3eeaf68fa220054f50391481
Bruno  de  Souza Picinini <brunosouzapicinini@gmail.com>
2020-04-21 22:00:54 -0300

    enviando funcao por props e utilizacao de state

--- src/App.js
+++ src/App.js
@@ -1,38 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './App.css';
 import Tabela from "./Tabela";
 
-const autores = [
-    {
-        nome: 'Paulo',
-        livro: 'React',
-        preco: '1000'
-    },
-    {
-        nome: 'Daniel',
-        livro: 'Java',
-        preco: '99'
-    },
-    {
-        nome: 'Marcos',
-        livro: 'Design',
-        preco: '150'
-    },
-    {
-        nome: 'Bruno',
-        livro: 'DevOps',
-        preco: '100'
-    }
-];
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Hello World!</h1>
-            <Tabela autores={autores}/>
-        </div>
-    );
+class App extends Component {
+
+    state = {
+        autores: [
+            {
+                nome: 'Paulo',
+                livro: 'React',
+                preco: '1000'
+            },
+            {
+                nome: 'Daniel',
+                livro: 'Java',
+                preco: '99'
+            },
+            {
+                nome: 'Marcos',
+                livro: 'Design',
+                preco: '150'
+            },
+            {
+                nome: 'Bruno',
+                livro: 'DevOps',
+                preco: '100'
+            }
+        ],
+    };
+
+    removeAutor = index => {
+
+        const {autores} = this.state;
+
+        this.setState({
+            autores: autores.filter((autor, posAtual) => {
+                return posAtual !== index;
+            })
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello World!</h1>
+                <Tabela autores={this.state.autores} removeAutor={this.removeAutor}/>
+            </div>
+        );
+    };
+
 }
 
 export default App;
