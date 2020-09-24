8de0507b9733b304bc2003479e9abef5c0e91e28
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2020-09-23 22:43:10 -0300

    Tipografia pagina sobre

--- src/Pages/Sobre/Sobre.js
+++ src/Pages/Sobre/Sobre.js
@@ -4,12 +4,19 @@ import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import {makeStyles} from "@material-ui/core/styles";
 
+const useEstilos = makeStyles({
+    titulo: {
+        textAlign: 'center',
+        color: 'blue'
+    }
+})
 const Sobre = () => {
+    const classes = useEstilos()
     return(
         <Fragment>
             <Header/>
             <Container maxWidth='sm'>
-                <Typography variant='h1' component='h2'>Sobre</Typography>
+                <Typography className={classes.titulo} variant='h1' component='h2'>Sobre</Typography>
                 <Typography variant='body1' component='p'>
                     A Casa do Código é uma editora que desenvolve e edita livros em diversos formatos.
                 </Typography>
